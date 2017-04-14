b400d00c8ed7bee563eb3f7da659bbcefa6f8a92
rosiehighsmith <ariel.highsmith@gmail.com>
2017-04-14 08:02:11 -0700

    Implemented expression-bodied property for ProductCode

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -73,9 +73,6 @@ namespace Acme.Biz
             }
         }
 
-        internal string Category { get; set; }
-        public int SequenceNumber { get; set; } = 1;
-
         public string ValidationMessage { get; set; }
 
         private string description;
@@ -108,6 +105,10 @@ namespace Acme.Biz
             set { productVendor = value; }
         }
 
+        public string Category { get; set; }
+        public int SequenceNumber { get; set; } = 1;
+
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
 
         public string SayHello()
         {
--- AcmeApp/Tests/Acme.BizTests/ProductTests.cs
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -255,5 +255,19 @@ namespace Acme.Biz.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ProductCode_DefaultValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            var expected = "Tools-1";
+
+            //Act
+            var actual = currentProduct.ProductCode;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
